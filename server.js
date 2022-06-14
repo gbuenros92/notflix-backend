e2740cf51614da3924c75dffbdad9e5ba4e6440f
Gustavo Buenrostro <g.buenrostro1992@gmail.com>
2022-06-14 16:19:48 -0400

    Initial commit:  created folder structures and User and Movie models

---
+++ server.js
@@ -0,0 +1,24 @@
+// Imports
+const express = require('express')
+const app = express()
+require('dotenv').config
+
+// ===== ROUTES =====
+// Index
+// app.use('/')
+
+// New
+
+// Delete
+
+// Update
+
+// Create
+
+// Edit
+
+// Show
+
+// ===== PORT =====
+const PORT = 8080
+app.listen(PORT, () => console.log(`Express app running on Port ${PORT}`))
