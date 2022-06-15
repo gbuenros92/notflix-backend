c641ed3538cdcd1c0d97609a2bd3e0187cc52118
Gustavo Buenrostro <g.buenrostro1992@gmail.com>
2022-06-15 12:24:05 -0400

    CRUD functionality on movies

--- controllers/api/movies.js
+++ controllers/api/movies.js
@@ -6,10 +6,43 @@ const index = async (req, res) => {
         const movies = await Movie.find({})
         res.status(200).json(movies)
     } catch (err) {
-        res.status(400).json({msg: err.message})
+        res.status(400).json({ msg: err.message })
+    }
+}
+
+// Create a movie
+const create = async (req, res) => {
+    try {
+        const createdMovie = await Movie.create(req.body)
+        res.status(200).json(createdMovie)
+    } catch (err) {
+        res.status(400).json({ msg: err.message })
+    }
+}
+
+// Update a movie
+const update = async (req, res) => {
+    try {
+        const updatedMovie = await Movie.findByIdAndUpdate(req.params.id, req.body, { new: true })
+        res.status(200).json(updatedMovie)
+    } catch (err) {
+        res.status(400).json({ msg: err.message })
+    }
+}
+
+// Delete a movie
+const remove = async (req, res) => {
+    try {
+        const deletedMovie = await Movie.findByIdAndDelete(req.params.id)
+        res.status(200).json(deletedMovie)
+    } catch (err) {
+        res.status(400).json({ msg: err.message })
     }
 }
 
 module.exports = {
-    index
+    index,
+    create,
+    update,
+    remove
 }
--- server.js
+++ server.js
@@ -2,7 +2,10 @@
 const express = require('express')
 const app = express()
 require('dotenv').config()
-require('./cofig/database')
+require('./config/database')
+
+// ===== MIDDLEWARES =====
+app.use(express.json())
 
 // ===== ROUTES =====
 // Movies
